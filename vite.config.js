2264cedb3552fea2a08a3b4e6a4119a7e4a4658c
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-03-20 19:53:11 -0600

    editted config to allworedirect for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync(resolve(__dirname, '_redirects'), resolve(__dirname, 'dist/_redirects'))
+      }
+    }
   ],
 })
