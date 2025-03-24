516db84244f5e28601be99f5a4b405933a310cf3
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-03-24 17:04:33 -0600

    updated import statement  in partnerbanner ToolTip to Tooltip

--- src/components/partners/PartnerBanner.jsx
+++ src/components/partners/PartnerBanner.jsx
@@ -10,7 +10,7 @@
 
 import React from "react";
 import { shuffledPartners } from "../../data/partnerData";
-import ToolTip from '../ui-utils/ToolTip';
+import ToolTip from '../ui-utils/Tooltip';
 
 
 function PartnerList() {
