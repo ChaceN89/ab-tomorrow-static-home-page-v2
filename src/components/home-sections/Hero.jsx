0d73a3fe1996b183331f0ad90f9b627790d8c51a
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-03-24 16:54:56 -0600

    updated the homeHero section

--- src/components/home-sections/Hero.jsx
+++ src/components/home-sections/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 // background
 import heroBanner from '../../assets/ui-elements/abtom-banner.jpg';
+import heroBannerSm from '../../assets/ui-elements/abtom-banner-sm.jpg';
 
 // Award logos
 import award1 from '../../assets/awards/28th-emerald-finalist-logo.png';
@@ -14,7 +15,7 @@ export default function Hero() {
       id="hero"
       className={`
         relative text-white flex justify-center items-end text-center
-        h-[80vh] lg:h-screen
+        h-[80vh] lg:h-screen lg:m-0
         bg-cover bg-no-repeat
         bg-center lg:bg-[calc(50%-7rem)_center] 
         lg:bg-fixed
@@ -22,10 +23,10 @@ export default function Hero() {
       style={{ backgroundImage: `url(${heroBanner})` }}
     >
       <div className="relative z-10 pb-14 lg:pb-24 space-y-4">
-        <div className="flex justify-center items-end gap-4 sm:gap-10 lg:gap-32">
+        <div className="flex justify-center items-end gap-2 sm:gap-12 md:gap-20 lg:gap-32">
           <img src={award1} alt="" className="h-28 sm:h-44 award-shadow" />
-          <img src={award2} alt="" className="h-28 sm:h-44 award-shadow" />
-          <img src={award3} alt="" className="h-28 sm:h-44 award-shadow" />
+          <img src={award2} alt="" className="h-28 sm:h-44 " />
+          <img src={award3} alt="" className="h-28 sm:h-44 " />
         </div>
         <a href="https://www.simulator.albertatomorrow.ca/" className="btn-blue">Launch Simulator</a>
       </div>
