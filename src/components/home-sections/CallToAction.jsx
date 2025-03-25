eceae2331edf3d466ce3a933f3c2280a9b5b23cf
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-03-25 17:40:38 -0600

    udpated call to action shadow

--- src/components/home-sections/CallToAction.jsx
+++ src/components/home-sections/CallToAction.jsx
@@ -47,7 +47,7 @@ export default function CallToAction() {
         </div>
 
         {/* Frosted Glass Content Card */}
-        <div className="bg-primary/20 backdrop-blur-lg rounded-2xl border border-white/20 shadow-xl p-8 w-full max-w-3xl space-y-6 large-shadow">
+        <div className="bg-primary/20 backdrop-blur-lg rounded-2xl border border-white/20 shadow-xl p-8 w-full max-w-3xl space-y-6 small-shadow">
           <h2 className="text-3xl lg:text-4xl font-bold uppercase text-white font-display ">
             Now set some goals and design a future for Alberta that you want to see!
           </h2>
